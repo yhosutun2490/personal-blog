9006d4f3ff3284c5c867ee5d297af399a6906205
yhosutun2491 <yhosutun2491@gmail.com>
2025-03-08 17:51:05 +0800

    feat- init project and package

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+<style>
+
+body {
+  background-color: #fff;
+  color: rgba(0,0,0,0.8);
+}
+.dark-mode body {
+  background-color: #091a28;
+  color: #ebf4f1;
+}
+.sepia-mode body {
+  background-color: #f1e7d0;
+  color: #433422;
+}
+
+</style>
---
+++ pages/about/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <h1>Color mode: {{ $colorMode.value }}</h1>
+    </div>
+  </template>
+  <script setup>
+  const colorMode = useColorMode()
+  
+  console.log(colorMode.preference)
+//   definePageMeta({
+//     colorMode: 'light',
+//  })
+  </script>
+  
